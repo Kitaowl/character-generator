82d978f6ee27ae55cb3f787175af16567d115cc2
Kitaowl <mykyta646@gmail.com>
2025-10-27 23:25:10 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,42 +1,9 @@
-// const character = {
-//     name: "Justynian",
-//     role: "Wizard",
-//     level: 1,
-//     skills: ["Fireball", "Healing field"],
-//     equipment: {
-//         weapon: "dagger",
-//         armor: "wooden armor",
-//         items: ["Healing potion", "Compass"]
-//     }
-// }
-
-
-// function createCharacter(name, role) {
-
-// }
-
-// function addItem(character, item) {
-
-// }
-
-// function learnSkill(character, newSkill) {
-
-// }
-
-// function levelUp(character) {
-
-// }
-
-// function levelUp(character) {
-
-// }
-
-function createCharacter(userName, userRole, userLevel) {
+function createCharacter(userName, userRole, userLevel, umiejetnosci) {
     return {
         level: userLevel,
         role: userRole,
         name: userName,
-        skills: [],
+        skills: umiejetnosci,
         equipment: { // nowy objekt
             weapon: "grey hammer",
             armor: "heavy plates",
@@ -44,7 +11,49 @@ function createCharacter(userName, userRole, userLevel) {
         }
     }
 }
+const mojaPostac = createCharacter("Grzmotosław", "Paladyn", 5, "Podnoszenie ciękich przedmiotów") // 1
 
-const mojaPostac = createCharacter("Grzmotosław", "Paladyn", 5, ) // 1
+function addItem(sword, shield, Iron_chest, Iron_boots, Iron_greaves, Iron_helmet) {
+    return {
+        weapon: sword,
+        defend: shield,
+        armor: {
+            head: Iron_helmet,
+            chest: Iron_chest,
+            legs: Iron_greaves,
+            feet: Iron_boots
+        }
+    }
+}
+const bron = addItem("Miecz", "Tarcza", "Żelazny chest", "Żelazne buty", "Żelazne greaves", "Żelazny helmet")
 
-console.log(mojaPostac);
+function learnSkill(character, newSkill) {
+    return {
+        userCharacter: character,
+        userNewSkill: newSkill
+        }
+    }
+const newSkill = learnSkill("Grzmotosław", "Teleport")
+
+function levelUp(character) {
+    return {
+        levelUp: character
+    }
+}
+const levelingUp = levelUp("Podwyzszenie postaci o jeden level")
+
+function characterDescription(character) {
+    return {
+        opis: character
+    }
+}
+const opisPostaci = characterDescription("Grzmotosław jest bardzo silną i waleczną postacią.")
+
+console.log(mojaPostac, bron, newSkill, levelingUp, opisPostaci)
+
+// function addItem(character, item) {
+//     const mojEkwipunek = addItem
+//     return {
+
+//     }
+// }
